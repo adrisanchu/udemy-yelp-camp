e57f6d7d05a223b4a921e72916041413f670c828
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-19 12:36:34 +0100

    setup - seeds - Campgrounds: generate random camprgrounds names in random cities by looping arrays

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+// function to get random result from an array, limited by its length
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// generate 50 new random campgrounds in our DB
+// by randomly looping through cities and seedHelpers
+const seedDB = async() => {
+    // first delete everything in our collection
+    await Campground.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            title: `${sample(descriptors)} ${sample(places)}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+// close connection once it's done
+seedDB().then(() => {
+    mongoose.connection.close();
+});
