5f1705878eaf66ce0156c0cba496b80df482d71b
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-25 16:00:54 +0100

    setup - error handling: moving campgroundSchema out of app and requiring it!

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -32,6 +33,19 @@ app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// Joi middleware for server side data validation
+const validateCampground = (req, res, next) => {
+    // destructuring the error from the Joi error object
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        // map through details array and join all messages separated by comma
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -41,20 +55,9 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
-app.post('/campgrounds', catchAsync(async(req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async(req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0)
-        }).required()
-    })
-    // destructuring the error from the Joi error object
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
