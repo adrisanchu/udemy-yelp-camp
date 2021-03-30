704f4654ab6409312335f75424d8a163c7859804
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-03-30 11:55:31 +0200

    MVC - V: Display /flash error messages when id not found

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,11 +38,19 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Sorry, the campground was not found');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Sorry, the campground was not found');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
