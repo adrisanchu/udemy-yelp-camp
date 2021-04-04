600c39b2c961b1570caf2b0c6ad246d5616daa87
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-04 18:51:27 +0200

    MVC - C: /login. Using passport to handle authentication as middleware.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -14,12 +15,25 @@ router.post('/register', catchAsync(async (req, res) => {
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
         // console.log(registeredUser);
-        req.flash('success', 'Welcome to Yelp Camp!');
+        req.flash('success', `Welcome to Yelp Camp, ${username}!`);
         res.redirect('/campgrounds');
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+// passport.authenticate middleware, passing Validation Strategy
+// it could be google, github, ...
+// we pass also settings to configure what to do during authentication
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    const { username } = req.body;
+    req.flash('success', `Welcome back, ${username}!`);
+    res.redirect('/campgrounds');
+});
 
 module.exports = router;
