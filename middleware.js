fc300df734dac74b3471ab9a7cf43de5568bff34
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-04 22:30:45 +0200

    setup - middleware: adding req.session.returnTo to know where to redirect after register!

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        // store the url of origin to know the last page!
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in.');
         return res.redirect('/login');
     }
--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,12 @@ router.post('/register', catchAsync(async (req, res) => {
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
         // console.log(registeredUser);
-        req.flash('success', `Welcome to Yelp Camp, ${username}!`);
-        res.redirect('/campgrounds');
+        // once we register, we use .login() method (from passport) to login
+        req.login(registeredUser, (err) => {
+            if (err) return next(err);
+            req.flash('success', `Welcome to Yelp Camp, ${username}!`);
+            res.redirect('/campgrounds');
+        });
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('register');
@@ -33,7 +37,12 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     const { username } = req.body;
     req.flash('success', `Welcome back, ${username}!`);
-    res.redirect('/campgrounds');
+    // get original URL from session (stored in middleware)
+    // if going to /login directly, then redirect to /campgrounds
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    // once this info used, we delete it from the session
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res) => {
