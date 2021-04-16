d7722f96c0c7abd09a61f22a046da9e24d31bed1
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-16 20:27:28 +0200

    MVC - C: /campgrounds refactoring. Moving middlewares out: isAuthor, isLoggedIn, validateCampground

--- middleware.js
+++ middleware.js
@@ -1,9 +1,38 @@
+const { campgroundSchema } = require('./schemas.js');
+const Campground = require('./models/campground');
+const ExpressError = require('./utils/ExpressError');
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         // store the url of origin to know the last page!
         req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in.');
         return res.redirect('/login');
+    }
+    next();
+};
+
+// Joi middleware for server side data validation
+module.exports.validateCampground = (req, res, next) => {
+    // destructuring the error from the Joi error object
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        // map through details array and join all messages separated by comma
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
+// middleware to check if currUser is the author of the campground
+module.exports.isAuthor = async(req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,36 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
-
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
-const campground = require('../models/campground');
 
-// Joi middleware for server side data validation
-const validateCampground = (req, res, next) => {
-    // destructuring the error from the Joi error object
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        // map through details array and join all messages separated by comma
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-};
-
-// middleware to check if currUser is the author of the campground
-const isAuthor = async(req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    next();
-};
 
 router.get('/', async(req, res) => {
     const campgrounds = await Campground.find({});
