c892d6dd6803fd392f358d1b7d6ec910ebe8e7e1
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-18 21:22:38 +0200

    MVC - C: middleware isReviewAuthor to check if the user can delete a review or not

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError');
+const Review = require('./models/review');
 
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -31,6 +32,18 @@ module.exports.isAuthor = async(req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if(!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
+
+// middleware to check if currUser is the author of the review (to control deletion)
+module.exports.isReviewAuthor = async(req, res, next) => {
+    // getting both campground id and review id
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
