df2384a7f6e05ebca21ec16898343ab6795ea46b
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-04-18 21:23:56 +0200

    MVC - C: /reviews. Check if user is logged in before making a new review, check auth before delete

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,13 +3,15 @@ const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     // create new review (independent from any campground)
     const review = new Review(req.body.review);
+    // add currentUser to review model as author
+    review.author = req.user._id;
     // attach the review to a campground in an array of reviews
     campground.reviews.push(review);
     await review.save();
@@ -18,7 +20,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:reviewId', catchAsync( async(req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync( async(req, res) => {
     const { id, reviewId } = req.params;
     // find the campground and remove the review
     // using the pull() method (see Mongo docs)
